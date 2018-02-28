07e06d79ab577094d3fdd6a3e327b3c0c5e4e6c9
yarik3351 <yarik3351@gmail.com>
2018-02-28 12:57:11 +0200

    i hate this shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'api/v1/invoices/', to: 'invoices#index'
   namespace 'api' do
     namespace 'v1' do
+      root 'invoices#index'
       resources :customers
       resources :users
       resources :invoices
